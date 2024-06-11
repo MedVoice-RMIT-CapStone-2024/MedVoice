23824815975bb9bf5c7d0328b3f68397137d4bd7
bbigemoloser <s3818552@rmit.edu.vn>
2024-06-11 19:56:39 +0700

    delete files and push

--- lib/domain/entities/profession/profession_info.dart
+++
@@ -1,8 +0,0 @@
-class ProfessionInfo {
-  String? mLong_name = "";
-  String? mShort_name = "";
-  String? mId = "";
-
-  ProfessionInfo(this.mLong_name, this.mShort_name, this.mId);
-  ProfessionInfo.buildDefault();
-}
