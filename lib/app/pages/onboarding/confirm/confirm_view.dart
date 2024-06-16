9466580b45d0a025fb3cb797fe32eaa994228ccf
bbigemoloser <s3818552@rmit.edu.vn>
2024-06-16 17:37:02 +0700

    delete old background and adjust to color theory

--- lib/app/pages/onboarding/confirm/confirm_view.dart
+++ lib/app/pages/onboarding/confirm/confirm_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_architecture/flutter_clean_architecture.dart' as clean;
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart'
+    as clean;
 import 'package:hexcolor/hexcolor.dart';
 import 'package:med_voice/app/assets/image_assets.dart';
 import 'package:med_voice/app/widgets/background_set.dart';
+import 'package:med_voice/app/widgets/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../common/base_controller.dart';
 import '../../../../common/base_state_view.dart';
@@ -36,85 +39,84 @@ class _ConfirmView extends BaseStateView<ConfirmView, ConfirmController> {
   Widget body(BuildContext context, BaseController controller) {
     ConfirmController _controller = controller as ConfirmController;
     final Size size = MediaQuery.of(context).size;
+    ThemeData theme = Provider.of<ThemeProvider>(context).themeData;
     return Scaffold(
-        backgroundColor: HexColor(Global.mColors['pink_2'].toString()),
-        body: BackgroundSetUp(
-          link: ImageAssets.imgBg5,
-          isShowLogo: false,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: toSize(20)),
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: toSize(250)),
-                    Image.asset(ImageAssets.imgEmail,
-                        width: toSize(190), height: toSize(190), fit: BoxFit.fill),
-                    Text("Check your Inbox",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: toSize(30),
-                          fontWeight: FontWeight.w800,
-                        )),
-                    Padding(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-                      child: Text(
-                        "An email containing instructions to reset your password has been sent to @dudu.com",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: toSize(13),
-                          fontWeight: FontWeight.w300,
-                        ),
+        backgroundColor: theme.colorScheme.background,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: toSize(20)),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: toSize(250)),
+                  Image.asset(ImageAssets.imgEmail,
+                      width: toSize(190),
+                      height: toSize(190),
+                      fit: BoxFit.fill),
+                  Text("Check your Inbox",
+                      style: TextStyle(
+                        color: theme.colorScheme.onBackground,
+                        fontSize: toSize(30),
+                        fontWeight: FontWeight.w800,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 70, vertical: 10),
+                    child: Text(
+                      "An email containing instructions to reset your password has been sent to @dudu.com",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: theme.colorScheme.onBackground,
+                        fontSize: toSize(13),
+                        fontWeight: FontWeight.w300,
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(toSize(10)),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          fixedSize: Size(size.width * 0.6, toSize(50)),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Text(
-                          "Open email app",
-                          style: TextStyle(
-                            color: HexColor("#FFFDF5"),
-                            fontSize: toSize(14),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: toSize(10)),
-                    Text("Resend email",
-                        style: TextStyle(
-                          color: HexColor("#EC4B8B"),
-                          fontSize: toSize(14),
-                          fontWeight: FontWeight.bold,
-                        )),
-                  ],
-                ),
-                Positioned(
-                  top: toSize(60),
-                  child: InkWell(
-                    onTap: () {
-                      onBack();
-                    },
-                    child: Image.asset(
-                      IconAssets.icBack,
-                      width: toSize(20),
-                      height: toSize(20),
                     ),
                   ),
+                  Padding(
+                    padding: EdgeInsets.all(toSize(10)),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: theme.colorScheme.primary,
+                        fixedSize: Size(size.width * 0.6, toSize(50)),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "Open email app",
+                        style: TextStyle(
+                          color: theme.colorScheme.surface,
+                          fontSize: toSize(14),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: toSize(10)),
+                  Text("Resend email",
+                      style: TextStyle(
+                        color: theme.colorScheme.onBackground,
+                        fontSize: toSize(14),
+                        fontWeight: FontWeight.bold,
+                      )),
+                ],
+              ),
+              Positioned(
+                top: toSize(60),
+                child: InkWell(
+                  onTap: () {
+                    onBack();
+                  },
+                  child: Image.asset(
+                    IconAssets.icBack,
+                    width: toSize(20),
+                    height: toSize(20),
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ));
   }
--- lib/app/widgets/small_text_field.dart
+++ lib/app/widgets/small_text_field.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:med_voice/app/widgets/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../utils/module_utils.dart';
 
@@ -11,17 +13,22 @@ class SmallTextField extends StatelessWidget {
     required this.validator,
     required this.controller,
     required this.showIconButton,
+    required this.fillColor,
     this.onChanged,
     this.obscureText = false,
     this.icon,
     this.iconButton,
     this.hint,
+    this.iconColor,
   }) : super(key: key);
   final String labelText;
+  final Color fillColor;
   final IconData? icon;
   final IconButton? iconButton;
   final bool showIconButton;
   final String? hint;
+  final Color? iconColor;
+
   final bool obscureText;
   final String? Function(String?)?
       validator; // Accepts a function for validation
@@ -30,6 +37,7 @@ class SmallTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Provider.of<ThemeProvider>(context).themeData;
     return Container(
       margin: EdgeInsets.symmetric(vertical: toSize(10)),
       padding: EdgeInsets.symmetric(horizontal: toSize(20)),
@@ -40,26 +48,28 @@ class SmallTextField extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
             child: Text(labelText,
                 style: TextStyle(
-                  color: Colors.black,
+                  color: theme.colorScheme.onBackground,
                   fontSize: toSize(11),
-                  fontWeight: FontWeight.w400,
+                  // fontWeight: FontWeight.w400,
                   letterSpacing: toSize(1.5),
                 )),
           ),
           TextFormField(
             onChanged: onChanged,
             obscureText: obscureText,
-            style: const TextStyle(
-              color: Colors.black,
+            style: TextStyle(
+              color: theme.colorScheme.onBackground,
               fontSize: 14,
-              fontWeight: FontWeight.w300,
+              // fontWeight: FontWeight.w300,
             ),
             decoration: InputDecoration(
                 suffixIcon: Padding(
                   padding: EdgeInsets.only(right: toSize(8.0)),
-                  child: showIconButton ? iconButton : Icon(icon),
+                  child: showIconButton
+                      ? iconButton
+                      : Icon(icon, color: iconColor),
                 ),
-                fillColor: HexColor("FBE8F2"),
+                fillColor: fillColor,
                 filled: true,
                 floatingLabelBehavior: FloatingLabelBehavior.always,
                 border: OutlineInputBorder(
@@ -69,15 +79,31 @@ class SmallTextField extends StatelessWidget {
                 ),
                 isDense: true,
                 hintText: hint,
-                hintStyle: const TextStyle(
-                  color: Colors.black,
+                hintStyle: TextStyle(
+                  color: theme.colorScheme.onBackground,
                   fontSize: 14,
                   fontWeight: FontWeight.w300,
                 ),
-                focusedBorder: OutlineInputBorder(
+                errorStyle: TextStyle(
+                  color: theme.colorScheme.onSecondary,
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: theme.colorScheme
+                        .onSecondary, // Change this to your desired color
+                    // Adjust the width if necessary
+                  ),
                   borderRadius:
                       BorderRadius.circular(toSize(20)), // Adjusted size
-                  borderSide: BorderSide(color: HexColor("#EC4B8B")),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: theme.colorScheme
+                        .onSecondary, // Change this to your desired color
+                    // Adjust the width if necessary
+                  ),
+                  borderRadius:
+                      BorderRadius.circular(toSize(20)), // Adjusted size
                 )),
             validator: validator, // Pass the validation function
             controller: controller,
