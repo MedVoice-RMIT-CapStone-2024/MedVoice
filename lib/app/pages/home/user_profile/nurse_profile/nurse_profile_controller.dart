77d795977873b09607c7f71cbab200b791ba8e93
phongho63 <phongho63@gmail.com>
2024-07-19 15:45:19 +0700

    Updated features: Improved MedVoice onboarding splash screen, biometric authentication feature

--- lib/app/pages/home/user_profile/nurse_profile/nurse_profile_controller.dart
+++ lib/app/pages/home/user_profile/nurse_profile/nurse_profile_controller.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../../common/base_controller.dart';
 
 class NurseProfileController extends BaseController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool toggleBioAuth = false;
+
   bool isShowStartButton = false;
   Timer? timer;
   ThemeMode themeMode = ThemeMode.system;
@@ -16,5 +21,83 @@ class NurseProfileController extends BaseController {
   void onListener() {}
 
   @override
-  void firstLoad() {}
+  void firstLoad() {
+    fetchPriorCredentials();
+  }
+
+  Future<void> saveCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('bioLoginEmail', emailController.text);
+    await prefs.setString('bioLoginPassword', passwordController.text);
+    view.showPopupWithAction(
+        'Email: ${emailController.text} \nPassword: ${passwordController.text}',
+        'Okay',
+        () {},
+        'Credentials saved');
+    refreshUI();
+    saveBioConfig();
+  }
+
+  Future<void> fetchCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? email = prefs.getString('loginEmail');
+    String? password = prefs.getString('loginPassword');
+    emailController.text = email ?? '';
+    passwordController.text = password ?? '';
+    debugPrint(
+        'Email: ${emailController.text} \nPassword: ${passwordController.text}');
+    refreshUI();
+    saveCredentials();
+  }
+
+  void onTurningOnBioAuth() {
+    view.showPopupWithAction(
+        'Enable Biometric Authentication for this account?', 'Ok', () {
+      fetchCredentials();
+      refreshUI();
+    }, 'Enable Bio Authentication', 'Cancel', () {});
+  }
+
+  void onTurningOffBioAuth() {
+    view.showPopupWithAction(
+        'Disable Biometric Authentication for this account?', 'Ok', () {
+      resetBioCredentials();
+      refreshUI();
+    }, 'Disable Bio Authentication', 'Cancel', () {});
+  }
+
+  Future<void> saveBioConfig() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('bioToggle', true);
+    toggleBioAuth = true;
+    refreshUI();
+  }
+
+  // Fetch both email storage and compare them to fetch the bio configurations
+  Future<void> fetchPriorCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? bioEmail = prefs.getString('bioLoginEmail');
+    String? email = prefs.getString('loginEmail');
+    if (email != null && bioEmail != null) {
+      if (bioEmail.isNotEmpty) {
+        if (bioEmail == email) {
+          if (prefs.getBool('bioToggle') != null) {
+            toggleBioAuth = prefs.getBool('bioToggle')!;
+          } else {
+            view.onGeneralError('Could not fetch data');
+          }
+        }
+      }
+    }
+    refreshUI();
+  }
+
+  Future<void> resetBioCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('bioLoginEmail', '');
+    await prefs.setString('bioLoginPassword', '');
+    await prefs.setBool('bioToggle', false);
+    toggleBioAuth = false;
+    refreshUI();
+  }
 }
--- lib/app/pages/onboarding/login/sign_in_view.dart
+++ lib/app/pages/onboarding/login/sign_in_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart'
     as clean;
+import 'package:local_auth/error_codes.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:lottie/lottie.dart';
 
-import 'package:hexcolor/hexcolor.dart';
 import 'package:med_voice/app/pages/onboarding/login/sign_in_controller.dart';
 import 'package:med_voice/app/utils/module_utils.dart';
 import 'package:med_voice/app/widgets/theme_provider.dart';
@@ -11,6 +13,7 @@ import 'package:provider/provider.dart';
 import '../../../../common/base_controller.dart';
 import '../../../../common/base_state_view.dart';
 
+import '../../../assets/lottie_assets.dart';
 import '../../../utils/pages.dart';
 import '../../../widgets/small_text_field.dart';
 import '../signup/sign_up_view.dart';
@@ -59,7 +62,7 @@ class _SignInView extends BaseStateView<SignInView, SignInController> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Spacer(),
+                  const Spacer(),
                   Text(
                     "Login",
                     style: TextStyle(
@@ -108,30 +111,42 @@ class _SignInView extends BaseStateView<SignInView, SignInController> {
                     controller: _controller.passwordController,
                   ),
                   SizedBox(height: toSize(5)),
-                  Padding(
-                    padding: EdgeInsets.all(toSize(10)),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_controller.submitForm()) {
-                          pushScreen(Pages.main, isAllowBack: false);
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: theme.colorScheme.primary,
-                        fixedSize: Size(size.width * 0.76, toSize(50)),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(toSize(10)),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_controller.submitForm()) {
+                              pushScreen(Pages.main, isAllowBack: false);
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: theme.colorScheme.primary,
+                            fixedSize: Size(size.width * 0.7, toSize(50)),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            "Log in",
+                            style: TextStyle(
+                              color: theme.colorScheme.surface,
+                              fontSize: toSize(18),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
                         ),
                       ),
-                      child: Text(
-                        "Log in",
-                        style: TextStyle(
-                          color: theme.colorScheme.surface,
-                          fontSize: toSize(18),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                      InkWell(
+                        onTap: () async {
+                          _controller.onLogInThroughBioAuth();
+                        },
+                        child: Container(margin: EdgeInsets.only(right: toSize(4)), child: Icon(Icons.fingerprint , size: toSize(40))),
+                      )
+                    ],
                   ),
                   SizedBox(height: toSize(10)),
                   InkWell(
