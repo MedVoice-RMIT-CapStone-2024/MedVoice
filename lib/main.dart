11d10b9a2d1d7004a12345574d8bbb20346cc437
phongho63 <phongho63@gmail.com>
2024-07-28 10:18:37 +0700

    feature: Adding feedback feature to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:feedback/feedback.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart'; // Import your theme data
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:med_voice/app/widgets/theme_provider.dart';
 import 'package:med_voice/restart_widget.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +14,20 @@ void main() async {
     // Add other configuration parameters as needed
   );
   Widget app = await initializeApp(devAppConfig);
-  runApp(ChangeNotifierProvider(
-      create: (context) => ThemeProvider(), child: RestartWidget(child: app)));
+  runApp(BetterFeedback(
+    theme: FeedbackThemeData(
+      background: Colors.grey,
+      feedbackSheetColor: Colors.grey[50]!,
+      drawColors: [Colors.red, Colors.green, Colors.blue, Colors.yellow],
+    ),
+    localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalFeedbackLocalizationsDelegate(),
+    ],
+    mode: FeedbackMode.draw,
+    child: ChangeNotifierProvider(
+        create: (context) => ThemeProvider(), child: RestartWidget(child: app)),
+  ));
 }
